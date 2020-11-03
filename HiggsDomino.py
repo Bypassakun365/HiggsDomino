7f6ce326d22f9319c006329eb1bfc7c06899078a
Husnul <72526264+hackeratjeh@users.noreply.github.com>
2020-11-03 16:03:12 +0700

    Higgsdomino.py
    pkg upgate && pkg upgrade
    pkg install python2
    pkg install git
    hit clone https://github.com/hackeratjeh/HiggsDomino
    ls Higgsdomino
    cd higgsdomino
    ls
    Python2 higgsdomino.py

---
+++ HiggsDomino.py
@@ -0,0 +1,14 @@
+#/bin/bash
+echo : Higgs domino island
+#domino island
+#scatter
+#super win 4.600.000.000
+#mega win 800.000.000
+#big win 300.000.000
+============================>
+#ID :
+#PASSWORD :
+============================>
+LOADING......../|\|/|/
+
+
